efaef7fc819f0a240120e478cc8a1b205f76f85a
Максим Рушманов <doublemax@mail.ru>
2022-02-07 23:57:05 +0400

    Initial commit

---
+++ src/test/java/ru/learnup/spring/demospringapp/DemoSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.learnup.spring.demospringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
